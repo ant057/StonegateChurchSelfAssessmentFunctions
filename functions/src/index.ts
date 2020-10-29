6a212ce5c4bc12d57ed89e9f334ec0a623118264
Anthony Cunningham <oxa547@gmail.com>
2020-10-28 19:46:28 -0500

    .

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,6 @@ import * as nodemailer from 'nodemailer';
 
 admin.initializeApp();
 
-const db = admin.firestore();
-
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -19,34 +17,31 @@ exports.emailPeerAssessmentContacts = functions.firestore.document('/peer-assess
         const data = snap.data();
         const emailAddress = data.emailAddress;
         const peerFullName = data.fullName;
-        const selfAssessmentId = data.selfAssessmentId;
-        let selfFullName = '';
-
-        db.doc(`self-assessments/${selfAssessmentId}`).get().then(doc => {
-            if(doc.exists) {
-                selfFullName = doc.data()?.selfUserFullName;
-            }
-        });
-
-        functions.logger.log('ok here now::' + selfFullName);
+        const selfFullName = data.selfUserFullName;
+        const link = 'http://' + data.linkToAssessment;
+        let d = new Date();
+        d.setDate(d.getDate() + 7);
 
         const mailOptions = {
             from: 'Tony Robinson Coaching - Peer Assessment <tonyrobinson.selfawareness@gmail.com>',
             to: emailAddress,
             subject: 'Tony Robinson Coaching - Peer Assessment request',
             html: `Hi, ${peerFullName}! <br><br> You have been requested to complete a peer self-awareness assessment for <b>${selfFullName}</b>.<br> If you could complete
-        this by ${new Date().toLocaleDateString()} that would be great! <br><br>Thank you so much for taking the time to assist in the leadership development of our 
+        this by ${d.toLocaleDateString()} that would be great! <br><br><a href="${link}">Click here to take your peer assessment!</a> <br><br>Thank you so much for taking the time to assist in the leadership development of our 
         brothers and sisters in Christ.` //Email content in HTML
         };
 
         // Access the parameter `{documentId}` with `context.params`
-        functions.logger.log('Starting Send E-mail for Peer-Assessment Contacts', context.params.documentId);
+        functions.logger.log('Starting Send E-mail for Peer-Assessment Contacts::', context.params.documentId);
 
         return transporter.sendMail(mailOptions, (erro, info) => {
             if (erro) {
-                functions.logger.error('Error sending e-mail', erro.toString());
+                functions.logger.error('Error sending e-mail::', erro.toString());
+                // if error - save to peer assess record?
+                // retry?
             }
             functions.logger.log('E-mail delivered!');
+            // save to perr asses record date sent
         });
 
         // You must return a Promise when performing asynchronous tasks inside a Functions such as
