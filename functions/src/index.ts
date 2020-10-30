19642d9a3c97b884fa1f77b9b18521f24c91c425
Anthony <oxa547@gmail.com>
2020-10-30 00:14:38 -0500

    ..

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as nodemailer from 'nodemailer';
+import * as cors from 'cors';
+const corsHandler = cors({ origin: true });
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -17,19 +19,15 @@ exports.emailPeerAssessmentContacts = functions.firestore.document('/peer-assess
     .onCreate((snap, context) => {
         const id = context.params.documentId;
         const data = snap.data();
-        const emailAddress = data.emailAddress;
-        const peerFullName = data.fullName;
-        const selfFullName = data.selfUserFullName;
-        const link = 'http://' + data.linkToAssessment;
         let d = new Date();
         d.setDate(d.getDate() + 7);
 
         const mailOptions = {
             from: 'Tony Robinson Coaching - Peer Assessment <tonyrobinson.selfawareness@gmail.com>',
-            to: emailAddress,
+            to: data.emailAddress,
             subject: 'Tony Robinson Coaching - Peer Assessment request',
-            html: `Hi, ${peerFullName}! <br><br> You have been requested to complete a peer self-awareness assessment for <b>${selfFullName}</b>.<br> If you could complete
-            this by ${d.toLocaleDateString()} that would be great! <br><br><a href="${link}">Click here to take your peer assessment!</a> <br><br>Thank you so much for taking the time to assist in the leadership development of our 
+            html: `Hi, ${data.fullName}! <br><br> You have been requested to complete a peer self-awareness assessment for <b>${data.selfUserFullName}</b>.<br> If you could complete
+            this by ${d.toLocaleDateString()} that would be great! <br><br><a href="${'http://' + data.linkToAssessment}">Click here to take your peer assessment!</a> <br><br>Thank you so much for taking the time to assist in the leadership development of our 
             brothers and sisters in Christ.` //Email content in HTML
         };
 
@@ -57,68 +55,42 @@ exports.emailPeerAssessmentContacts = functions.firestore.document('/peer-assess
     });
 
 
-exports.emailPeerAssessmentContactsReminder = functions.https.onCall((data, context) => {
-    // Message text passed from the client.
-    functions.logger.log(data);
-    //const reminder: any[] = data.reminder;
-    // Authentication / user information is automatically added to the request.
-    // const uid = context.auth?.uid || null;
-    // const name = context.auth?.token.name || null;
-    // functions.logger.log('Curious here::', uid + ' ' + name);
+exports.emailPeerAssessmentContactsReminder = functions.https.onRequest((req, res) => {
+    corsHandler(req, res, () => {
+        const reminder = req.body;
 
-    //let retReminder: any[] = [];
-    return data;
+        reminder.forEach((element: any) => {
+            const mailOptions = {
+                from: 'Tony Robinson Coaching - Peer Assessment <tonyrobinson.selfawareness@gmail.com>',
+                to: element.emailAddress,
+                subject: 'Tony Robinson Coaching - Peer Assessment reminder',
+                html: `Hi, ${element.fullName}! <br><br> We want to send you a friendly reminder to complete your peer self-awareness assessment for <b>${element.selfUserFullName}</b>.<br><br>
+            <a href="${'http://' + element.linkToAssessment}">Click here to take your peer assessment!</a> <br><br>Thank you so much for taking the time to assist in the leadership development of our 
+            brothers and sisters in Christ.` //Email content in HTML
+            };
 
-    // reminder.forEach((element: any) => {
-    //     let peerAssessmentId = element.peerAssessmentId;
-    //     let peerFullName = element.fullName;
-    //     let link = element.LinkToAssessment;
-    //     let selfFullName = element.selfUserFullName;
-    //     let emailAddress = element.emailAddress;
+            functions.logger.log('Starting Send E-mail for Peer-Assessment Contacts::', element.peerAssessmentId);
 
-    //     const mailOptions = {
-    //         from: 'Tony Robinson Coaching - Peer Assessment <tonyrobinson.selfawareness@gmail.com>',
-    //         to: emailAddress,
-    //         subject: 'Tony Robinson Coaching - Peer Assessment reminder',
-    //         html: `Hi, ${peerFullName}! <br><br> We want to send you a friendly reminder to complete your peer self-awareness assessment for <b>${selfFullName}.<br><br>
-    //         <a href="${link}">Click here to take your peer assessment!</a> <br><br>Thank you so much for taking the time to assist in the leadership development of our 
-    //         brothers and sisters in Christ.` //Email content in HTML
-    //     };
+            transporter.sendMail(mailOptions, (erro, info) => {
+                if (erro) {
+                    functions.logger.error('Error sending e-mail::', erro.toString());
+                    db.doc(`/peer-assessments/${element.peerAssessmentId}`).set({
+                        lastMailError: erro.toString(),
+                        lastMailDate: null
+                    }, { merge: true });
+                }
+                else {
+                    functions.logger.log('E-mail delivered!');
+                    db.doc(`/peer-assessments/${element.peerAssessmentId}`).set({
+                        lastMailError: '',
+                        lastMailDate: new Date().toLocaleDateString()
+                    }, { merge: true });
+                }
+            });
+        });
 
-    //     functions.logger.log('Starting Send E-mail for Peer-Assessment Contacts::', peerAssessmentId);
-
-    //     transporter.sendMail(mailOptions, (erro, info) => {
-    //         if (erro) {
-    //             functions.logger.error('Error sending e-mail::', erro.toString());
-    //             db.doc(`/peer-assessments/${peerAssessmentId}`).set({
-    //                 lastMailError: erro.toString(),
-    //                 lastMailDate: null
-    //             }, { merge: true });
-
-    //             retReminder.push({
-    //                 peerAssessmentId: peerAssessmentId,
-    //                 peerFullName: peerFullName,
-    //                 successMail: false,
-    //                 error: erro.toString()
-    //             })
-    //         }
-    //         else {
-    //             functions.logger.log('E-mail delivered!');
-    //             db.doc(`/peer-assessments/${peerAssessmentId}`).set({
-    //                 lastMailError: '',
-    //                 lastMailDate: new Date().toLocaleDateString()
-    //             }, { merge: true });
-
-    //             retReminder.push({
-    //                 peerAssessmentId: peerAssessmentId,
-    //                 peerFullName: peerFullName,
-    //                 successMail: true
-    //             })
-    //         }
-
-    //         return retReminder;
-    //     });
-    // });
+        res.send('done');
+    });
 });
 
 // Add email sent date to peer assessment record
